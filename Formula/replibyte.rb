3944d59a1126ae6db49f6fe9b3412d142dbf7206
Romaric Philogène <rphilogene@qovery.com>
2022-05-06 10:21:18 +0200

    Update replibyte.rb

--- Formula/replibyte.rb
+++ Formula/replibyte.rb
@@ -3,7 +3,7 @@ class Replibyte < Formula
   desc "Synchronize your cloud databases and hide sensitive data 🕵️‍♀️"
   homepage "https://github.com/Qovery/replibyte"
   url "https://github.com/Qovery/replibyte/archive/refs/tags/v#{version}.zip"
-  sha256 "9d242e9d5765f25d7e7c4a4be691500c690ed94a2393dc171b92c4765ea25727"
+  sha256 "71e8e89be6b36ea52699f67ed0ff324c53e4b96eeda34338db207be920b03378"
   license "MIT"
 
   def install
