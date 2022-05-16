2a68cc259b4b3a54fb8c8b3022868bb4b911c020
Romaric Philogène <rphilogene@qovery.com>
2022-05-16 22:53:52 +0200

    Update replibyte.rb

--- Formula/replibyte.rb
+++ Formula/replibyte.rb
@@ -2,7 +2,7 @@ class Replibyte < Formula
   version "0.6.3"
   desc "Seed Your Development Database With Real Data ⚡️"
   homepage "https://github.com/Qovery/replibyte"
-  url "https://github.com/Qovery/replibyte/archive/refs/tags/v#{version}.zip"
+  url "https://github.com/Qovery/replibyte/releases/download/v#{version}/replibyte_v#{version}_x86_64-apple-darwin.zip"
   sha256 "db1bed1a7a94ed2d594f4ba9f1613a04bed1e2f606c2af77a2f8fa709c8bbc9d"
   license "MIT"
 
