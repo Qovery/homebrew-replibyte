8fe8b824a5e84e648776bf5a7933768a7be9346b
Romaric Philogène <rphilogene@qovery.com>
2022-08-14 11:57:24 +0200

    replibyte 0.9.7
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/replibyte.rb
+++ Formula/replibyte.rb
@@ -1,9 +1,9 @@
 class Replibyte < Formula
-  version "0.9.6"
+  version "0.9.7"
   desc "Seed Your Development Database With Real Data ⚡️"
   homepage "https://github.com/Qovery/replibyte"
-  url "https://github.com/Qovery/replibyte/releases/download/v0.9.6/replibyte_v0.9.6_x86_64-apple-darwin.zip"
-  sha256 "5acb8448669cab2ea6778396e60dcd8e0117b294e69b0f6aaf4b8151ae2e838f"
+  url "https://github.com/Qovery/replibyte/releases/download/v0.9.7/replibyte_v0.9.7_x86_64-apple-darwin.zip"
+  sha256 "5a425ab62cf32f9cddd53ba165fdda302c5c9e4554b77ae8be6ae4a05e2652b8"
   license "MIT"
 
   def install
